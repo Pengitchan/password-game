3cf5db336c968eebb244f49a4a8ce3f9d36232c9
hugoknvr2008 <hkneveler@notre-dame-reze.fr>
2025-01-23 13:40:00 +0100

    historique
    code pour avoir l'historique des mot de passe dans un txt

---
+++ def historique_password():.py
@@ -0,0 +1,7 @@
+def historique_password():
+    '''reecupere les mots de passe géneré et les ecrit dans un .txt
+        entrée (str) le mot de passe 
+        sorties un fichier texte '''
+        f = open('historique.txt', 'a') 
+        f. write(reponse )
+        return  
